ab1bdef383d58535959a69d851438c43a7cb2c1a
Olivier Kermorgant <olivier_kermorgant@yahoo.fr>
2023-01-06 17:56:49 +0100

    rviz config for ekf

--- test/ekf_launch.py
+++ test/ekf_launch.py
@@ -5,12 +5,20 @@ from os import path
 def generate_launch_description():
     
     sl = SimpleLauncher()
+    ns = sl.declare_arg('namespace', 'bluerov2')
 
     base_dir = path.abspath(path.dirname(__file__))
 
     sl.include('bluerov2_description', 'state_publisher_launch.py')
 
-    sl.node('robot_localization', 'ekf_node',
+    with sl.group(ns=ns):
+        sl.node('robot_localization', 'ekf_node',
             parameters=[base_dir + '/' + 'ekf.yaml'])
+
+    base_link = ns + '/base_link'
+    odom = ns + '/odom'
+    sl.node('tf2_ros', 'static_transform_publisher',
+                name = 'odom',
+                arguments = ['--frame-id', odom, '--child-frame-id', base_link])
     
     return sl.launch_description()
--- test/rviz_launch.py
+++ test/rviz_launch.py
@@ -2,23 +2,31 @@ from simple_launch import SimpleLauncher
 from os import path
 from math import pi
 
+sl = SimpleLauncher()
+sl.declare_arg('imu','mpu')
 
-def generate_launch_description():
-    
-    sl = SimpleLauncher()
 
-    imu = sl.declare_arg('imu','mpu')
+def launch_setup():
 
+    imu = sl.arg('imu')
     base_dir = path.dirname(__file__)
 
-    sl.rviz(path.join(base_dir, 'imu.rviz'))
+    if imu == 'ekf':
+        # fused imu, description already here
+        sl.rviz(path.join(base_dir, 'ekf.rviz'))
+    else:
+        # basic imu test
+        sl.rviz(path.join(base_dir, 'imu.rviz'))
 
-    sl.include('bluerov2_description', 'state_publisher_launch.py')
+        sl.include('bluerov2_description', 'state_publisher_launch.py')
 
-    sl.node('pose_to_tf', 'pose_to_tf', parameters={'topic': imu})
+        sl.node('pose_to_tf', 'pose_to_tf', parameters={'topic': imu})
 
-    sl.node('tf2_ros', 'static_transform_publisher',
+        sl.node('tf2_ros', 'static_transform_publisher',
                 name = imu + '_tf',
                 arguments = ['--frame-id', imu] + f'--child-frame-id bluerov2/base_link --y -0.1 --yaw {pi/2}'.split())
     
     return sl.launch_description()
+
+
+generate_launch_description = sl.launch_description(launch_setup)
