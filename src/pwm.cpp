3e9e0b0b5059c1d30a615b2113aeddcaf8a789df
Olivier Kermorgant <olivier_kermorgant@yahoo.fr>
2026-04-30 16:53:03 +0200

    adapting to BlueROV2 + BlueOS + ROS 2 extension

--- src/pwm.cpp
+++ src/pwm.cpp
@@ -3,7 +3,7 @@
 #include <sensor_msgs/msg/joint_state.hpp>
 #include <std_msgs/msg/bool.hpp>
 #include <std_msgs/msg/float32.hpp>
-#include <navio2_ros/pwm.hpp>
+#include <mavros_msgs/msg/override_rc_in.hpp>
 
 namespace blurr
 {
@@ -11,66 +11,74 @@ namespace blurr
 using sensor_msgs::msg::JointState;
 using std_msgs::msg::Bool;
 using std_msgs::msg::Float32;
-using navio2_ros::PWM;
+using mavros_msgs::msg::OverrideRCIn;
 using namespace std::chrono_literals;
 
 // special pins
-constexpr auto thruster_pins{std::array{0,2,4,6,8,10}};
-constexpr auto light_pin{13};
-constexpr auto tilt_pin{12};
+constexpr auto thruster_pins{std::array{0,1,2,3,4,5}};
+constexpr auto light_pin{9};
+constexpr auto tilt_pin{7};
 
-// velocity -> pwm map from BlueRobotics @ 16 V
-constexpr auto velocities{std::array{-362.82,-336.69,-299.99,-265.21,-224.11,-180.25,-130.49,-63.83,0.0,0.0,63.0,129.54,180.06,222.91,263.67,302.43,340.46,370.02}};
-constexpr auto pwm{std::array{1100.f, 1150.f, 1200.f, 1250.f, 1300.f, 1350.f, 1400.f, 1450.f, 1480.f, 1520.f, 1550.f, 1600.f, 1650.f, 1700.f, 1750.f, 1800.f, 1850.f, 1900.f}};
-static_assert (velocities.size() == pwm.size(), "vel/pwm map should have same sizes");
+// thrust -> pwm map from BlueRobotics @ 16 V
+constexpr auto pwm{std::array{ 1100.00f,1150.00f,1200.00f,1250.00f,1300.00f,1350.00f,1400.00f,1450.00f,1480.00f,1520.00f,1550.00f,1600.00f,1650.00f,1700.00f,1750.00f,1800.00f,1850.00f,1900.00f }};
+//constexpr auto vels{std::array{ 362.81f,336.68f,299.98f,265.20f,224.11f,180.25f,130.49f,63.83f,0.00f,0.00f,63.00f,129.54f,180.06f,222.91f,263.67f,302.42f,340.46f,370.01f }};
+constexpr auto thrusts{std::array{ -39.92f,-34.20f,-26.56f,-20.14f,-14.11f,-9.06f,-4.72f,-1.13f,0.00f,0.00f,1.38f,5.92f,11.41f,17.89f,25.65f,33.55f,43.61f,51.45f }};
+
+static_assert (thrusts.size() == pwm.size(), "vel/pwm map should have same sizes");
+static_assert (thruster_pins.size() == 6, "thruster pins should be size 6");
 
 constexpr auto watchdog_period{1};
 
 float interpPWM(double v)
 {
   // no extrapolation
-  if(v <= velocities.front())
+  if(v <= thrusts.front())
     return pwm.front();
-  else if(v >=velocities.back())
+  else if(v >= thrusts.back())
     return pwm.back();
 
-  uint i = 0;
-  while ( v > velocities[i+1] ) i++;
+  uint i{0};
+  while ( v > thrusts[i+1] ) i++;
 
-  const auto xL = velocities[i], xR = velocities[i+1];
-  const auto yL = pwm[i], yR = pwm[i+1];
+  const auto xL{thrusts[i]}, xR{thrusts[i+1]};
+  const auto yL{pwm[i]}, yR{pwm[i+1]};
   return yL + ( yR - yL ) / ( xR - xL ) * ( v - xL );
 }
-
-
 
 class BlurrPWM : public rclcpp::Node
 {
 public:
   explicit BlurrPWM(rclcpp::NodeOptions options) :
-    rclcpp::Node("pwm", options),
-    pwm(this, {0, 2, 4, 6, 8, 10, tilt_pin, light_pin}),
-    pwm_timer{create_wall_timer(20ms, [&](){toPWM();})},
-    thruster_watchdog{create_wall_timer(std::chrono::seconds(watchdog_period), [&](){watchDog();})}
-  { 
-    run_sub = create_subscription<Bool>("run", 10, [&](Bool::UniquePtr msg){running = msg->data;});
-
-    tilt_sub = create_subscription<JointState>("joint_setpoint", 10, [&](JointState::UniquePtr msg)
-    {readTilt(*msg);});
-
-    thruster_sub = create_subscription<JointState>("thruster_command", 10, [&](JointState::UniquePtr msg)
-    {readThrusters(*msg);});
-    light_sub = create_subscription<Float32>("light", 10, [&](Float32::UniquePtr msg) {light_intensity = msg->data;});
+      rclcpp::Node("pwm", options)
+  {
+    pwm_pub = create_publisher<OverrideRCIn>("rc/override", 2);
 
     tilt_pub = create_publisher<JointState>("joint_states", rclcpp::SensorDataQoS());
     tilt_msg.name = {"tilt"};
     tilt_msg.position = {0};
+
+    run_sub = create_subscription<Bool>("run", 10, [&](Bool::UniquePtr msg){running = msg->data;});
+
+    tilt_sub = create_subscription<JointState>("joint_setpoint", 10, [&](JointState::UniquePtr msg)
+                                               {cmd_tilt_cb(*msg);});
+
+    thruster_sub = create_subscription<JointState>("cmd_thrust", 10, [&](JointState::UniquePtr msg)
+                                                   {cmd_thrust_cb(*msg);});
+    light_sub = create_subscription<Float32>("light", 10, [&](Float32::UniquePtr msg) {light_intensity = msg->data;});
+
+
   }
 
-private:    
+private:
 
-  PWM pwm;
-  rclcpp::TimerBase::SharedPtr pwm_timer, thruster_watchdog;
+  inline auto& tilt_angle()
+  {
+    return tilt_msg.position[0];
+  }
+
+  rclcpp::TimerBase::SharedPtr
+      pwm_timer{create_wall_timer(20ms, [&](){publish();})},
+      thruster_watchdog{create_wall_timer(std::chrono::seconds(watchdog_period), [&](){watchDog();})};
 
   rclcpp::Subscription<Bool>::SharedPtr run_sub;
   bool running{true};
@@ -78,34 +86,37 @@ private:
   rclcpp::Subscription<Float32>::SharedPtr light_sub;
   rclcpp::Subscription<JointState>::SharedPtr tilt_sub, thruster_sub;
 
-  rclcpp::Publisher<JointState>::SharedPtr tilt_pub;
-  JointState tilt_msg;
 
+  // received reference
   std::array<double, 6> thruster_force{0};
   double thruster_time{};
-
-  double tilt_angle{0};
+  JointState tilt_msg;
   float light_intensity{0};
+  rclcpp::Publisher<JointState>::SharedPtr tilt_pub;
+
+  // bridge to RC override
+  OverrideRCIn pwm;
+  rclcpp::Publisher<OverrideRCIn>::SharedPtr pwm_pub;
 
   // callbacks
 
-  void readTilt(const JointState &msg)
+  void cmd_tilt_cb(const JointState &msg)
   {
     if(msg.name.size() && msg.position.size())
-      tilt_angle = std::min(0.785, std::max(-0.785, msg.position[0]));
+      tilt_angle() = std::clamp(msg.position[0], -M_PI_4, M_PI_4);
   }
 
-  void readThrusters(const JointState &msg)
+  void cmd_thrust_cb(const JointState &msg)
   {
     thruster_time = now().seconds();
-    const static std::vector<std::string> names{"thruster0","thruster1","thruster2","thruster3","thruster4","thruster5"};
+    const static std::vector<std::string> names{"thruster1","thruster2","thruster3","thruster4","thruster5","thruster6"};
     for(uint i = 0; i<msg.name.size();++i)
     {
       for(uint j = 0; j<6; ++j)
       {
         if(msg.name[i] == names[j])
         {
-          thruster_force[j] = msg.velocity[i];
+          thruster_force[j] = msg.effort[i];
           break;
         }
       }
@@ -115,27 +126,30 @@ private:
   void watchDog()
   {
     if(now().seconds() - thruster_time > watchdog_period)
-      pwm.stop();
+    {
+      for(auto pin: thruster_pins)
+        pwm.channels[pin] = 1500;
+      tilt_angle() = 0;
+    }
   }
 
-  void toPWM()
+  void publish()
   {
     // at least remind the tilt angle
-    tilt_msg.position[0] = tilt_angle;
     tilt_msg.header.stamp = now();
     tilt_pub->publish(tilt_msg);
 
     if(!running)  return;
 
-    // thrusters    
+    // thrusters
     for(uint i = 0; i < thruster_pins.size(); ++i)
-      pwm.set_duty_cycle(thruster_pins[i], interpPWM(thruster_force[i]));
+      pwm.channels[thruster_pins[i]] = interpPWM(thruster_force[i]);
 
     // tilt: pwm + joint_states
-    pwm.set_duty_cycle(tilt_pin, 1500 + 509.3*tilt_angle);
+    pwm.channels[tilt_pin] = 1500 + 509.3*tilt_angle();
 
     // lumen light
-    pwm.set_duty_cycle(light_pin, 1100 + 800*light_intensity);
+    pwm.channels[light_pin] = 1100 + 800*light_intensity;
   }
 };
 
@@ -149,3 +163,4 @@ int main(int argc, char** argv)
   rclcpp::shutdown();
   return 0;
 }
+
